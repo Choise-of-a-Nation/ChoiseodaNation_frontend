e7390dd6bb2850b143e3ef4bb76873c41e22529e
Vlad <115177420+MelnikVladyslav@users.noreply.github.com>
2025-01-25 11:48:08 +0200

    Update

---
+++ my-app/src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from '../Components/Header';
+import './Home.css';
+
+function Home() {
+
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
+
+export default Home;
